dfde7198885b95884ba8ad6b3c42010f0af607a2
Mikey Kentaro <mikeykentarocho@gmail.com>
2018-06-14 20:00:52 -0700

    tests

--- public/js/src/test/publish.test.js
+++ public/js/src/test/publish.test.js
@@ -1,9 +1,9 @@
 import { shallow } from '../../../../node_modules/vue-test-utils'
-import App from '../components/App'
+import Publish from '../components/ads/publish'
 
-describe('App', () => {
+describe('Publish', () => {
   // Now mount the component and you have the wrapper
-  const wrapper = shallow(App)
+  const wrapper = shallow(Publish)
   // it's also easy to check for the existence of elements
   it('has a div', () => {
     expect(wrapper.contains('div')).toBe(true)
